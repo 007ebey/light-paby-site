f837bf4b2cfa6f17cae47497d94e5b2a080977f5
007ebey <007ebey@gmail.com>
2025-12-02 00:35:25 +0530

    lesseonNewLine

--- httpfromtcp/main.go
+++ httpfromtcp/main.go
@@ -6,34 +6,45 @@ import ("fmt"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
 	file, err := os.Open("messages.txt")
 	if err != nil {
       fmt.Println("Error:", err)
 	  return
 	}
-	// close as per pleasure
+	
 	defer file.Close()
-	// we need a buffer, slice of 8 bytes
+
 	buffer := make([]byte, 8)
 
+	// Persistent variable
+	var line string
 	// infinite for loop
 	for {
-       /*
-	     number of bytes actually read, error
-		 if any, data stored as reference
-	   */ 
-	   // Step 1 completed
 	   n, err := file.Read(buffer)
-	   // Step 3 completed
-	   if err == io.EOF {
-		  // no semicolon required
+	   if err == io.EOF { 
+		  fmt.Printf("read: %s\n", "end")
 		  break
 	   }
-	   // Step 2, slice the characters read
-	   toPrint := string(buffer[:n])
-	   fmt.Printf("read: %s\n", toPrint)
+	   // Decoupled here, detecting nl
+       part := -1
+	   for i, b := range buffer[:n] {
+		if b == '\n' {
+            part = i + 1
+		}
+	   }
 
+	   // there is a new line ( first line works great!)
+	   if part > -1 {
+		 // only update part of line
+		 line += string(buffer[:part - 1])
+		
+		 fmt.Printf("read: %s\n", line)
+		 // need to read the other part lol, forgot string!
+		 line = string(buffer[part:n])
+	   } else {
+		 // keep updating the line
+		 line += string(buffer[:n])
+	   }
 
 	   if err != nil {
 		// oops forgot double quotes
