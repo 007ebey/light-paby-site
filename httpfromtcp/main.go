14e3d37cc073a8b1d5edffbdcb157a535596e9b5
007ebey <007ebey@gmail.com>
2025-12-25 02:30:21 +0530

    revisit1

--- httpfromtcp/main.go
+++ httpfromtcp/main.go
@@ -2,9 +2,23 @@ package main
 
 import (
   "fmt"
+  "os"
+  "io"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println("Failed to open")
+	}
+	buff := make([]byte, 8)
+	defer file.Close()
+	for {
+		_, err := file.Read(buff)
+		fmt.Printf("read: %s\n", buff)
 
+		if err == io.EOF {
+			break
+		}
+	}
 }
