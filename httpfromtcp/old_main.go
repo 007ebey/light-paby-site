24361541d7f54774a790432f1c6b3b0b8525b6be
007ebey <007ebey@gmail.com>
2025-12-28 19:19:24 +0530

    message

---
+++ httpfromtcp/old_main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+  "fmt"
+  "io"
+  "net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer ln.Close()
+	for {
+		conn, err := ln.Accept()
+		fmt.Println("Conection has been accepted")
+		if err != nil {
+          fmt.Println(err)
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	for line := range getLinesChannel(conn) {
+		fmt.Println(line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <- chan string {
+	ch := make(chan string)
+	go func() {
+		defer close(ch)
+		var line []byte
+		buffer := make([]byte, 8)
+		for {
+			n, err := f.Read(buffer)
+			for _, b := range buffer[:n] {
+				if b == '\n' {
+	                ch <- "read: " + string(line)
+                    line = line[:0]
+				} else {
+					line = append(line, b)
+				}
+			}
+
+			if err == io.EOF {
+			    break
+		    }
+
+			if err != nil {
+				return
+			}
+		}
+
+		if len(line) > 0 {
+			ch <- "read: " + string(line)
+		}
+	}()
+
+	return ch
+}
